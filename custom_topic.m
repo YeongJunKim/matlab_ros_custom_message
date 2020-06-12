678d05bf57f8ab56b00f6f93c5a659bfe500a9af
colson <dud3722000@naver.com>
2020-06-12 16:43:56 +0900

    .

--- custom_topic.m
+++ custom_topic.m
@@ -5,3 +5,10 @@ rosgenmsg(folderpath)
 userFolder = '/home/colson/workspace/matlab_ros_custom_message/custom_msgs';
 addpath('/home/colson/catkin_ws/src/matlab_gen/msggen');
 
+
+
+%% step 1
+
+%% step 2
+addpath('/home/colson/catkin_ws/src/matlab_gen/msggen')
+savepath
